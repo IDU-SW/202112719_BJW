173c9ee154000d243ba0f6b4e7c09f3f73cd4dca
배진우 <bjwkor@naver.com>
2021-06-19 10:07:47 +0900

    add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : undefined,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev'),
+        DB_PORT: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        USERNAME: Joi.string().required(),
+        PASSWORD: Joi.string().required(),
+        DB: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
