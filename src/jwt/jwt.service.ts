e069478d35d9cb21780f9eb3e1b5c012b2466260
배진우 <bjwkor@naver.com>
2021-06-20 09:14:38 +0900

    add jwt middleware module

---
+++ src/jwt/jwt.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { sign, verify } from 'jsonwebtoken';
+import { JwtSign } from './interface/jwt-sign.interface';
+@Injectable()
+export class JwtService {
+  async sign(user: JwtSign): Promise<string> {
+    try {
+      const token = await sign({ ...user }, process.env.JWT_SECRET);
+
+      return token;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
